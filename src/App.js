d7457d92d2dd528d11fa5c58b41f5fc96e579997
Rachel Powe <rachelpowe@gmail.com>
2020-07-14 22:29:34 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ class App extends React.Component {
   // emailChange() {
   //   console.log("hi i am es5")
   // }
-  onEmailChange = (event) => {
-    console.log("hi, i email")
-  }
+  // onEmailChange = (event) => {
+  //   console.log("hi, i email")
+  // }
   render() {
     return(
       <div>
@@ -44,17 +44,39 @@ class App extends React.Component {
             value={this.state.birthday}
           />
           <form>
+            <label>Gender: </label>
             <label for="female">Female</label>
               <input
                 type="radio" 
                 id="female"
                 value={this.state.gender}></input>
-                <label for="female">Male</label>
+                <label for="male">Male</label>
               <input
                 type="radio" 
-                id="female"
+                id="male"
                 value={this.state.gender}></input>
           </form>
+          <form>
+          <label>Spayed or Neutered? : </label>
+            <label for="spayed">Spayed</label>
+              <input
+                type="radio" 
+                id="spayed"
+                value={this.state.spayedOrNeutered}></input>
+                <label for="neutered">Neutered</label>
+              <input
+                type="radio" 
+                id="neutered"
+                value={this.state.spayedOrNeutered}></input>
+          </form>
+            <form> //onSubmit="/action_page.php"//>
+                <label for="weight">Weight: </label>
+                <select id="weight" name="">
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="fiat">Fiat</option>
+                  <option value="audi">Audi</option>
+              </form>
           <button
             type="submit"
           >
